6a5cd723a70b823d79012df3f9cd7fff2cb1feac
Nox Prime <45508777+Nonoxprime@users.noreply.github.com>
2019-01-03 17:08:21 +0100

    Waiting Bar

--- WaitBar.ps1
+++ WaitBar.ps1
@@ -12,4 +12,4 @@ $process = $ProcessError.count}
 # -ErrorVariable ProcessError : Permet de mettre un valeur si il y a une erreur
 # $process = $ProcessError.count : crée une variable qui compte le nombre d’erreur.
 # La boucle « while » attend d’avoir une variable $Process = 1 pour pouvoir s’ouvrir. Donc si mon process ne tourne plus, la variable $process prend la valeur de 1.
-# $test n’a que pour but de ne pas afficher la valeur rendu par Get-process.
+# $test n’a que pour but de ne pas afficher la valeur rendu par Get-process
