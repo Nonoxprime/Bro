bfca186556b3e8694d9b4d127613a50242b1e50a
Nox Prime <45508777+Nonoxprime@users.noreply.github.com>
2019-01-08 07:03:50 +0100

    Delete Compilation.ps1

--- App/test/Compilation.ps1
+++
@@ -1,8 +0,0 @@
-$inputfile = Read-Host "Where is you Script ? "
-$outputfile = Read-Host "Compilation Name ? "
-$iconfile = Read-host "Where is your Ico ? (leave blank if don't) "
-
-if ($iconfile -eq $null) {$ico = ""}
-else {$ico = $iconfile }
-
-.\Labo\ps2exe.ps1 -inputFile $inputfile -outputFile $outputfile -iconFile $ico
